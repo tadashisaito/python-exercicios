7c09bf8cb40049c276def7fc010b12a8f657c101
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-26 20:11:41 -0300

    Add program to sum two integers
    Implementa um programa que lê dois números inteiros e exibe a soma.

---
+++ 01-tipos-primitivos/ex003.py
@@ -0,0 +1,11 @@
+"""
+Exercício 03 – Soma de dois números
+
+Programa que lê dois números inteiros e exibe a soma entre eles.
+"""
+
+n1 = int (input('Digite um número: '))
+n2 = int (input('Digite um outro número: '))
+soma = n1 + n2
+
+print (f'A soma entre {n1} + {n2}, é igual a {soma}')
