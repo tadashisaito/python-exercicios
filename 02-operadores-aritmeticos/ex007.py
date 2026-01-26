514aafab1abdfa3d7617d0846cbb230607332ee5
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-26 20:20:16 -0300

    Add program to calculate student's average score
    Implementa um programa para calcular a média de duas notas.

---
+++ 02-operadores-aritmeticos/ex007.py
@@ -0,0 +1,12 @@
+"""
+Exercício 07 – Média do aluno
+
+Programa que lê duas notas de um aluno
+e calcula a sua média.
+"""
+
+n1 = float (input ('Digite a sua primeira nota: '))
+n2 = float (input ('Digite a sua segunda nota: '))
+media = (n1 + n2) / 2
+
+print (f'Com base na sua primeira nota {n1:.1f}, e a segunda nota {n2:.1f} \nA sua média é igual a {media:.1f}')
