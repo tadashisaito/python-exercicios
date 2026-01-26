3d22f50cb5bcdd31c70b2fb7ee56e72f26d1c73c
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-26 20:17:36 -0300

    Add program to find predecessor and successor of a number

---
+++ 02-operadores-aritmeticos/ex005.py
@@ -0,0 +1,12 @@
+"""
+Exercício – Antecessor e sucessor
+
+Programa que lê um número inteiro e mostra
+seu antecessor e seu sucessor.
+"""
+
+n = int (input ('Digite um valor: '))
+antecessor = n - 1
+sucessor = n + 1
+
+print (f'Analisando o valor {n}, e seu antecessor é o {antecessor}, o seu sucessor é o {sucessor}')
