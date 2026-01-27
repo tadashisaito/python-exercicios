157dbf5c78eb98c0faa66438096b1740429bba08
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-27 19:56:14 -0300

    Add currency conversion from reais to dollars

--- 02-operadores-aritmeticos/ex010.py
+++ 02-operadores-aritmeticos/ex010.py
@@ -3,7 +3,11 @@ Exercício 10 – Conversão de moeda
 
 Programa que lê a quantidade de dinheiro em reais
 e calcula quantos dólares podem ser comprados,
-considerando a cotação fixa.
+considerando a cotação fixa. (US$ 1,00 = R$ 3,27)
 """
 
 
+real = float (input ('Quanto dinheiro você tem na Carteira?R$ '))
+dolar = real / 3.27
+
+print (f'Com R$ {real:.2f}, você pode comprar US$ {dolar:.2f} ')
