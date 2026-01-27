95daeab37632327a0d77356ed0b7df71b1fddf2d
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-27 19:56:43 -0300

    Add wall area and paint requirement calculation

--- 02-operadores-aritmeticos/ex011.py
+++ 02-operadores-aritmeticos/ex011.py
@@ -6,4 +6,11 @@ calcula sua área e a quantidade de tinta necessária
 para pintá-la.
 """
 
+largura = float (input ('Largura da parede: '))
+altura = float (input ('Altura da parede: '))
+area = altura * largura
+tinta = area/2
+
+print (f'Sua parede tem a dimensão da sua parede é de {largura}x{altura}, a sua área é de {area}m²')
+print (f'Para pintar essa parede, você precisará de {tinta}L de tinta')
 
