b607134bbdc4eb59a02d72819f7de748de29155b
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-27 19:57:05 -0300

    Update ex012.py

--- 02-operadores-aritmeticos/ex012.py
+++ 02-operadores-aritmeticos/ex012.py
@@ -4,3 +4,8 @@ Exercício 12 – Desconto de produto
 Programa que lê o preço de um produto e calcula
 seu novo valor com desconto.
 """
+
+preço: float = float (input ('Digite o preço do produto? R$  '))
+novo = preço - (preço * 5/100)
+
+print (f'O produto que custava R$ {preço:.2f}, na promoção com 5% de desconto, sai por R$ {novo:.2f}')
