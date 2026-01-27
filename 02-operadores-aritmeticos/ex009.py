d670af58a3cf6b69a6e8052b4be79581ee6d5d20
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-27 19:55:13 -0300

    Add multiplication table for a given integer

--- 02-operadores-aritmeticos/ex009.py
+++ 02-operadores-aritmeticos/ex009.py
@@ -5,4 +5,18 @@ Programa que lê um número inteiro e mostra
 sua tabuada.
 """
 
+n = int (input ('Digite um número para ver a sua tabuada: '))
 
+print (f'De acordo com o valor {n} apresentado, a tabuada correspondente é:')
+print ('=-'*10)
+print (f'{n} x  {1} = {n*1}')
+print (f'{n} x  {2} = {n*2}')
+print (f'{n} x  {3} = {n*3}')
+print (f'{n} x  {4} = {n*4}')
+print (f'{n} x  {5} = {n*5}')
+print (f'{n} x  {6} = {n*6}')
+print (f'{n} x  {7} = {n*7}')
+print (f'{n} x  {8} = {n*8}')
+print (f'{n} x  {9} = {n*9}')
+print (f'{n} x {10} = {n*10}')
+print ('=-'*10)
