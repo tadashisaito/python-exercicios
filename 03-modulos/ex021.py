c6cc8f0b2cdba7de0b5e705c469b7ec49778565f
R. Pereira dos Santos <ruancarlos.0397@gmail.com>
2026-01-28 22:42:15 -0300

    Add audio playback functionality in ex021.py
    Implement audio playback using playsound module.

---
+++ 03-modulos/ex021.py
@@ -0,0 +1,12 @@
+"""
+Exercício 21 – Reproduzir áudio
+
+Programa que reproduz um arquivo de áudio MP3
+utilizando um módulo externo.
+"""
+
+from playsound3 import playsound
+
+# O arquivo MP3 não está incluído no repositório.
+# Para testar, adicione um arquivo MP3 na mesma pasta.
+playsound("ex021.mp3")
